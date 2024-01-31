0c42d36a008e67b1912ebcb26c2672fa05eb60b8
oajwah <o.ajwah1@gmail.com>
2024-01-31 14:51:16 +0300

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('#Row3 .cell a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+
+
+
+
